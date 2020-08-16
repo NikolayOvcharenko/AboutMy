d5f0a43fc480755e2ec6034aec412a4d7f4e5794
Nikolay <56501319+NikolayOvcharenko@users.noreply.github.com>
2020-08-16 09:59:09 +0300

    About My

---
+++ app/src/main/java/com/example/aboutmy/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.aboutmy
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import com.example.aboutmy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        
+       //findViewById<Button>(R.id.done_button).setOnClickListener {
+       //     addNickName(it) }
+
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+}
